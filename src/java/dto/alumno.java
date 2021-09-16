47434799cc43d5551513f387cdba67604419acd1
juanstar <grattpotter@gmail.com>
2021-09-15 19:57:57 -0500

    Version 1.2

---
+++ src/java/dto/alumno.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dto;
+
+/**
+ *
+ * @author j_sta
+ */
+public class alumno {
+
+    int id;
+    int idMateria;
+    String userName;
+    String apellidoPaterno;
+    String apellidoMaterno;
+    String estatus;
+
+}
---
+++ src/java/dto/grado.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dto;
+
+/**
+ *
+ * @author j_sta
+ */
+public class grado {
+
+    int id;
+    int idCarrera;
+    String nombre;
+    String estatus;
+}
